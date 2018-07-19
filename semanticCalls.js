cbe3eb801e9624317185cec81b658668d9a93cce
Roman Wang <rwang@sail.triad.local>
2018-07-19 12:41:49 -0400

    fixed date typing

--- semanticCalls.js
+++ semanticCalls.js
@@ -1,6 +1,25 @@
 $('.ui.fluid.normal.dropdown')
 .dropdown();
-$('.effects-calendar').calendar();
 $('.ui.checkbox')
     .checkbox()
 ;
+
+$('.effects-calendar').calendar({
+    type: 'date',
+    formatter: {
+          date: function (date, settings) {
+              if (!date) return '';
+              var day = date.getDate() + '';
+              if (day.length < 2) {
+                  day = '0' + day;
+              }
+              var month = (date.getMonth() + 1) + '';
+              if (month.length < 2) {
+                  month = '0' + month;
+              }
+              var year = date.getFullYear();
+              return year + '-' + month + '-' + day;
+          }
+      }
+
+});
--- submit.js
+++ submit.js
@@ -2,32 +2,35 @@
 var roommateKeys = ["c4um", "OData__x0076_rd7", "Gender", "GenderPreference", "State", "wvxs", "OData__x0066_o03", "Employment_x0020_Status", "tpqq", "Smoker", "Pets", "u9vz", "Additional"];
 var housingKeys = ["x7yh","gxla","t75w","meae","OData__x0077_qh3","OData__x0076_ro3","Smoker","Pets","Parking","GenderPreference","HousingType","Description","OData__x0061_o65","OData__x0070_ja0","hiyi"]
 var carPoolKeys = ["Location", "v42w", "E_x002d_Mail", "gwgz", "wg0m", "Additional", "d6ny", "espt"]
+
 var calendarKeys = ["wvxs", "OData__x0066_o03", "OData__x0077_qh3", "OData__x0076_ro3"]
 var numericKeys = ["tpqq","gxla","t75w","meae","v42w","d6ny","espt"];
-var SPLists = {"RoomMate":roommateKeys , "Housing":housingKeys, "CarPool": carPoolKeys};
-var boolKeys = {"Smoker", "Pets", "Parking"};
+var boolKeys = ["Smoker", "Pets", "Parking"];
 
- function getFieldValue(fieldId) {
+var SPLists = {"RoomMates":roommateKeys , "Housing":housingKeys, "CarPool": carPoolKeys};
+
+
+ function getFieldValue(fieldID) {
     // 'get field' is part of Semantics form behavior API
-    var fieldValue = $('.ui.form').form('get field', fieldId).val();
+    var fieldValue = $('.ui.form').form('get field', fieldID).val();
     if(boolKeys.includes(fieldID)){
       return (fieldValue === "on" ? true : false);
     }
     if(calendarKeys.includes(fieldID)){
       //TODO FIX DUMMY VALUE
-      return "2018-07-19T13:50:17Z";
+      return fieldID + "T00:00:00Z";
     }
     if(numericKeys.includes(fieldID)){
-      //TODO FIX DUMMY VALUE
       return parseFloat(fieldValue);
     }
-    return $('.ui.form').form('get field', fieldId).val();
+    return $('.ui.form').form('get field', fieldID).val();
  }
 
  function getFormJSON(keys) {
    var formJSON = {};
    for(var i = 0; i< keys.length; i++){
      formJSON[keys[i]] = getFieldValue(keys[i]);
+     console.log(keys[i]);
    }
    return formJSON;
  }
